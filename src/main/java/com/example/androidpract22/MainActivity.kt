b22268311af0f6b0819b88b2d2d931fcaf144aee
ClassZak <156465942+ClassZak@users.noreply.github.com>
2024-11-23 12:33:39 +0300

    college update

--- src/main/java/com/example/androidpract22/MainActivity.kt
+++ src/main/java/com/example/androidpract22/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.androidpract22
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
---
+++ src/main/java/com/example/androidpract22/SplashScreen.kt
@@ -0,0 +1,56 @@
+package com.example.androidpract22
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var animatedVector:AnimatedVectorDrawableCompat
+    private lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        runSplashScreen()
+    }
+    private fun runSplashScreen() {
+        val splashScreenDuration: Long = 1700
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreen,
+                MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }, splashScreenDuration)
+
+
+        animatedVector= AnimatedVectorDrawableCompat.create(this,R.drawable.hello_android_waving)!!
+        imageView=findViewById<ImageView>(R.id.imageObject)
+        imageView.setImageDrawable(animatedVector)
+
+        animatedVector.start()
+
+        animatedVector.registerAnimationCallback(object : Animatable2Compat.AnimationCallback(){
+            override fun onAnimationEnd(drawable: Drawable?) {
+                Handler(Looper.getMainLooper()).postDelayed({},1000)
+            }
+        })
+    }
+}
